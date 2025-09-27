5658238378f776c0b82d4a3261d188684de65519
iMohamedSheta <mohamed15.sheta15@gmail.com>
2025-09-27 12:17:47 +0300

    fix: fixed the way aggregate function convert the returned value to the user so it can work right like model binding types

--- execute_aggregate.go
+++ execute_aggregate.go
@@ -2,7 +2,6 @@ package xqb
 
 import (
 	"fmt"
-	"strconv"
 
 	xqbErr "github.com/iMohamedSheta/xqb/shared/errors"
 )
@@ -20,8 +19,8 @@ func (qb *QueryBuilder) Count(column string) (int64, error) {
 		return 0, fmt.Errorf("%w: Count() expected one row as result, got %d rows", xqbErr.ErrInvalidResult, len(data))
 	}
 
-	count, err := asInt64(data[0]["count"])
-	if err != nil {
+	count, ok := toInt64(data[0]["count"])
+	if !ok {
 		return 0, fmt.Errorf("%w: Count() failed to convert result value to int, %v", xqbErr.ErrInvalidResult, err)
 	}
 
@@ -41,8 +40,8 @@ func (qb *QueryBuilder) Avg(column string) (float64, error) {
 		return 0, fmt.Errorf("%w: Avg() expected one row as result, got %d rows", xqbErr.ErrInvalidResult, len(data))
 	}
 
-	avg, err := asFloat64(data[0]["avg"])
-	if err != nil {
+	avg, ok := toFloat64(data[0]["avg"])
+	if !ok {
 		return 0, fmt.Errorf("%w: Avg() failed to convert result value to float, %v", xqbErr.ErrInvalidResult, err)
 	}
 
@@ -62,8 +61,8 @@ func (qb *QueryBuilder) Sum(column string) (float64, error) {
 		return 0, fmt.Errorf("%w: Sum() expected one row as result, got %d rows", xqbErr.ErrInvalidResult, len(data))
 	}
 
-	sum, err := asFloat64(data[0]["sum"])
-	if err != nil {
+	sum, ok := toFloat64(data[0]["sum"])
+	if !ok {
 		return 0, fmt.Errorf("%w: Sum() failed to convert result value to float, %v", xqbErr.ErrInvalidResult, err)
 	}
 
@@ -83,8 +82,8 @@ func (qb *QueryBuilder) Min(column string) (float64, error) {
 		return 0, fmt.Errorf("%w: Min() expected one row as result, got %d rows", xqbErr.ErrInvalidResult, len(data))
 	}
 
-	min, err := asFloat64(data[0]["min"])
-	if err != nil {
+	min, ok := toFloat64(data[0]["min"])
+	if !ok {
 		return 0, fmt.Errorf("%w: Min() failed to convert result value to float, %v", xqbErr.ErrInvalidResult, err)
 	}
 
@@ -104,8 +103,8 @@ func (qb *QueryBuilder) Max(column string) (float64, error) {
 		return 0, fmt.Errorf("%w: Max() expected one row as result, got %d rows", xqbErr.ErrInvalidResult, len(data))
 	}
 
-	max, err := asFloat64(data[0]["max"])
-	if err != nil {
+	max, ok := toFloat64(data[0]["max"])
+	if !ok {
 		return 0, fmt.Errorf("%w: Max() failed to convert result value to float, %v", xqbErr.ErrInvalidResult, err)
 	}
 
@@ -140,54 +139,4 @@ func (qb *QueryBuilder) MinSql(column string) (string, []any, error) {
 func (qb *QueryBuilder) MaxSql(column string) (string, []any, error) {
 	qb.columns = []any{fmt.Sprintf("MAX(%s) as max", qb.Wrap(column))}
 	return qb.GetSql()
-}
-
-// asInt64 - Converts a value to an int64
-func asInt64(value any) (int64, error) {
-	switch v := value.(type) {
-	case int64:
-		return v, nil
-	case float64:
-		return int64(v), nil
-	case int:
-		return int64(v), nil
-	case int32:
-		return int64(v), nil
-	case uint64:
-		return int64(v), nil
-	case []uint8:
-		str := string(v)
-		n, err := strconv.ParseInt(str, 10, 64)
-		if err != nil {
-			return 0, fmt.Errorf("invalid int64 value from []byte %q: %v", str, err)
-		}
-		return n, nil
-	default:
-		return 0, fmt.Errorf("unsupported type %T for numeric conversion", v)
-	}
-}
-
-// asFloat64 - Converts a value to a float64
-func asFloat64(value any) (float64, error) {
-	switch v := value.(type) {
-	case float64:
-		return v, nil
-	case float32:
-		return float64(v), nil
-	case int:
-		return float64(v), nil
-	case int64:
-		return float64(v), nil
-	case uint64:
-		return float64(v), nil
-	case []uint8:
-		str := string(v)
-		n, err := strconv.ParseFloat(str, 64)
-		if err != nil {
-			return 0, fmt.Errorf("invalid float64 value from []byte %q: %v", str, err)
-		}
-		return n, nil
-	default:
-		return 0, fmt.Errorf("unsupported type %T for float64 conversion", v)
-	}
 }
